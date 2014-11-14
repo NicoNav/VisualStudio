b3146fc64b04e8c0230777ca52b2c0570c06f888
NicoNav <nicolasnavier@gmail.com>
2014-11-14 14:01:32 -0500

    Create controller.js
    ajout de controller

---
+++ Scripts/controller.js
@@ -0,0 +1,20 @@
+var phonecatApp = angular.module('phonecatApp',[]);
+
+
+phonecatApp.controller('PhoneListCtrl',function($scope){
+$scope.phones = [{
+'name' : 'Nexus 5 ',
+'snippet' : 'machin truc'}
+
+
+
+}
+
+
+
+
+];
+
+
+
+});
