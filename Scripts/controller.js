816dade411bef4017ae4629ee9fecbb884523f14
NicoNav <nicolasnavier@gmail.com>
2014-11-14 14:36:21 -0500

    Update controller.js

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -1,12 +1,10 @@
 var phonecatApp = angular.module('phonecatApp',[]);
 
 
-phonecatApp.controller('PhoneListCtrl',function($scope){
-  $http.get('/phones/phones.json').success(function(data) {
+phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
     $scope.phones = data;
   });
 
-  $scope.orderProp = 'age';
-
-
+  $scope.orderPhone = 'age';
 });
