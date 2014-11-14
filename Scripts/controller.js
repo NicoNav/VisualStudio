ab4da274e4a23192806641270eec9ea6d4024fd6
NicoNav <nicolasnavier@gmail.com>
2014-11-14 14:55:54 -0500

    Update controller.js

--- Scripts/controller.js
+++ Scripts/controller.js
@@ -4,7 +4,7 @@ var phonecatApp = angular.module('phonecatApp',[]);
 phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
   var obj = {content:null};
 
-    $http.get('phones.json').success(function(data) {
+    $http.get('phones/phones.json').success(function(data) {
         // you can do some processing here
         obj.content = data;
         $scope.phones = data;
